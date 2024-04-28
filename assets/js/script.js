2989b32057548f7cb178c20b0118e717dc787697
Matt Miles <matt_miles2@live.co.uk>
2024-04-28 15:39:46 +0100

    Update division functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,7 +50,6 @@ function runGame(gameType) {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
-
 }
 
 /**
@@ -131,7 +130,7 @@ function displayMultiplyQuestion(operand1, operand2) {
 }
 
 function displayDivisionQuestion(operand1, operand2) {
-    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand1").textContent = operand1 * operand2;
     document.getElementById("operand2").textContent = operand2;
     document.getElementById("operator").innerHTML = "/";
 }
